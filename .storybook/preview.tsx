62c3bdb0f0df57aa51abb010a0535180c9843258
ChaoqiYin <305111632@qq.com>
2023-12-01 22:40:21 +0800

    feat: odoo17内样式引入，避免样式冲突

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,11 @@
 import type { Preview } from '@storybook/react';
 import './styles/index.scss';
+// @ts-ignore
+import odooCssContent from '!!raw-loader!./styles/web.assets_web.min.css';
+
+const styleEl = document.createElement('style');
+styleEl.textContent = odooCssContent;
+document.head.appendChild(styleEl);
 
 const preview: Preview = {
     parameters: {
