06a9b1ed97084a1d27054fcb1a6b0ab2649156ad
ChaoqiYin <305111632@qq.com>
2023-12-04 14:12:00 +0800

    feat: inputNumber参数

--- src/components/input-number/InputNumber.ts
+++ src/components/input-number/InputNumber.ts
@@ -7,7 +7,6 @@ import './style/input-number.scss';
 import classNames from 'classnames';
 import { CompRef } from '@/hooks/useImperativeHandle';
 import useControllableState from '@/hooks/useControllableState';
-import { formatNumberCode } from '@/components/_util/stringUtils';
 import BigNumber from 'bignumber.js';
 
 const inputNumberWrapClass = getPrefixCls('input-number-wrap');
@@ -15,19 +14,15 @@ const inputNumberClass = getPrefixCls('input-number');
 const iconClass = getPrefixCls('input-number-icon');
 const numberHandlerWrapClass = getPrefixCls('input-number-handler-wrap');
 
-const eyeSVG = getSDSVG(_upSVG, {
+const upSVG = getSDSVG(_upSVG, {
     width: '1em',
     height: '1em'
 });
 
-const eyeCloseSVG = getSDSVG(_downSVG, {
+const downSVG = getSDSVG(_downSVG, {
     width: '1em',
     height: '1em'
 });
-
-type State = {
-    focused: boolean;
-}
 
 type Props = {
     step?: number;
@@ -35,7 +30,7 @@ type Props = {
     value?: number;
     onFocus?: (event: FocusEvent) => void;
     onBlur?: (event: FocusEvent) => void;
-    onChange?: (value: number) => void;
+    onChange?: (value: number | string) => void;
     max?: number;
     min?: number;
     disabled?: boolean;
@@ -43,7 +38,21 @@ type Props = {
     autoFocus?: boolean;
     changeOnBlur?: boolean;
     controls?: boolean;
+    decimalSeparator?: string;
+    precision?: number;
+    formatter?: (value: string) => string;
+    parser?: (value: string) => string;
+    keyboard?: boolean;
+    readonly?: boolean;
+    stringMode?: boolean;
+    onPressEnter?: (event: Event) => void;
+    onStep?: (value: number, info: { offset: number; type: 'up' | 'down' }) => void;
     slots?: Record<string, any>;
+}
+
+type State = {
+    focused: boolean;
+    formatValue: string;
 }
 
 class InputNumber extends Component<Props, State> {
@@ -51,6 +60,7 @@ class InputNumber extends Component<Props, State> {
         autoFocus: false,
         changeOnBlur: true,
         controls: true,
+        keyboard: true,
         step: 1,
         defaultValue: 0,
         max: Number.MAX_SAFE_INTEGER,
@@ -62,20 +72,27 @@ class InputNumber extends Component<Props, State> {
     static inputTemplate = `
 <div t-att-class="getClasses()">
     <Input className="" ref="inputRef" onFocus.bind="onFocus" onBlur.bind="onBlur" 
-        value="controllableState.state.value" onChange.bind="onchangeValue"
+        onKeyDown.bind="onKeyDown"
+        readonly="props.readonly"
+        onPressEnter="props.onPressEnter"
+        value="state.formatValue" onChange.bind="onchangeValue"
         disabled="props.disabled"
         size="props.size"
         bordered="false"
         slots="filterSlots('ix')"
     />
-    <t t-if="!props.disabled">
+    <t t-if="showControls()">
         <t t-set="iconClass" t-value="renderIconClasses()"/>
         <span class="${numberHandlerWrapClass}" t-ref="handlerRef" t-on-click="onClickWrap">
-            <span t-att-class="iconClass.increase" t-on-click="() => this.increaseOrDecrease(true)">
-                ${eyeSVG}
+            <span t-att-class="iconClass.increase" t-on-click="(event) => this.onStep('up', event)">
+                <t t-slot="upIcon">
+                    ${upSVG}
+                </t>
             </span>
-            <span t-att-class="iconClass.decrease" t-on-click="() => this.increaseOrDecrease(false)">
-                ${eyeCloseSVG}
+            <span t-att-class="iconClass.decrease" t-on-click="(event) => this.onStep('down', event)">
+                <t t-slot="downIcon">
+                    ${downSVG}
+                </t>
             </span>
         </span>
     </t>    
@@ -104,11 +121,12 @@ class InputNumber extends Component<Props, State> {
     `;
 
     state = useState<State>({
-        focused: false
+        focused: false,
+        formatValue: ''
     });
 
     controllableState = useControllableState(this.props, {
-        value: `${this.props.defaultValue}` || '0'
+        value: this.precisionValue(BigNumber(`${this.props.defaultValue}` || '0'))
     });
 
     inputRef: CompRef = { current: undefined };
@@ -121,18 +139,41 @@ class InputNumber extends Component<Props, State> {
         return !!(slots?.addonBefore || slots?.addonAfter);
     }
 
+    /**
+     * 过滤掉slots
+     * @param type 如果是addon，则过滤掉suffix、prefix，如果是ix，则过滤掉addonBefore、addonAfter
+     * @protected
+     */
     protected filterSlots(type: 'addon' | 'ix') {
         if (this.props.slots) {
             return omit(this.props.slots, type === 'addon' ? ['suffix', 'prefix'] : ['addonBefore', 'addonAfter']);
         }
     }
 
+    /**
+     * 是否显示加减按钮
+     * @protected
+     */
+    protected showControls(): boolean {
+        return !!this.props.controls && !this.props.disabled;
+    }
+
+    /**
+     * 获取焦点回调
+     * @param event
+     * @protected
+     */
     protected onFocus(event: FocusEvent): void {
         const { onFocus } = this.props;
         this.state.focused = true;
         onFocus?.(event);
     }
 
+    /**
+     * 失去焦点回调，如果changeOnBlur为true，则在失去焦点时触发onChange
+     * @param event
+     * @protected
+     */
     protected onBlur(event: FocusEvent): void {
         const { onBlur, changeOnBlur } = this.props;
         this.state.focused = false;
@@ -142,29 +183,89 @@ class InputNumber extends Component<Props, State> {
         }
     }
 
+    /**
+     * 键盘按下回调，如果keyboard为true，则可以使用上下键控制数字增减
+     * @param event
+     * @protected
+     */
+    protected onKeyDown(event: KeyboardEvent): void {
+        const { keyboard } = this.props;
+        if (keyboard) {
+            switch (event.key.toLowerCase()) {
+                case 'arrowup':
+                    event.preventDefault();
+                    this.increaseOrDecrease(true, event);
+                    break;
+                case 'arrowdown':
+                    event.preventDefault();
+                    this.increaseOrDecrease(false, event);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 点击加减按钮回调
+     * @param type up为加，down为减
+     * @param event
+     * @protected
+     */
+    protected onStep(type: 'up' | 'down', event: Event): void {
+        const { onStep } = this.props;
+        const oldValue = this.controllableState.state.value;
+        this.increaseOrDecrease(type === 'up', event);
+        onStep?.(Number(oldValue), { offset: this.props.step as number, type });
+    }
+
+    /**
+     * 点击外层wrap回调，聚焦input
+     * @protected
+     */
     protected onClickWrap(): void {
         this.inputRef.current?.focus();
     }
 
-    protected onchangeValue(value: string): void {
-        const number = BigNumber(formatNumberCode(value));
-        const v = number.isNaN() ? '0' : number.toFixed();
-        this.controllableState.setState({
-            value: v
-        });
-        this.props.onChange?.(Number(v));
+    /**
+     * 精确数字小数点并返回对应的字符串
+     * @param bn 值的BigNumber实例
+     * @protected
+     */
+    protected precisionValue(bn: BigNumber): string {
+        const { precision } = this.props;
+        return precision ? bn.toFixed(precision) : bn.toFixed();
     }
 
+    /**
+     * 输入框值改变回调
+     * @param value
+     * @protected
+     */
+    protected onchangeValue(value: string): void {
+        const { stringMode } = this.props;
+        let bn = BigNumber(this.parse(value));
+        bn = bn.isNaN() ? BigNumber('0') : bn;
+        let stringValue = this.precisionValue(bn);
+        this.controllableState.setState({
+            value: stringValue
+        });
+        this.props.onChange?.(stringMode ? stringValue : Number(stringValue));
+    }
+
+    /**
+     * 获取不超出范围的值，根据value值和max、min值判断
+     * @param value
+     * @protected
+     */
     protected getValueNotOutOfRange(value: string): string {
         const { max, min } = this.props;
-        const v = BigNumber(value)
+        let bn = BigNumber(value);
+        const v = bn.isNaN() ? BigNumber('0') : bn;
         if (v.isNaN() || v.isGreaterThan(max!)) {
             return max!.toString();
         }
         if (v.isNaN() || v.isLessThan(min!)) {
             return min!.toString();
         }
-        // todo: 统一小数位数
         return v.toFixed();
     }
 
@@ -185,18 +286,21 @@ class InputNumber extends Component<Props, State> {
     }
 
     /**
-     * 增加或减少数字
+     * 增加或减少value值
      * @param isIncrease 加true，减false
+     * @param event
      * @protected
      */
-    protected increaseOrDecrease(isIncrease: boolean): void {
+    protected increaseOrDecrease(isIncrease: boolean, event: Event): void {
+        event?.stopPropagation();
+        event?.preventDefault();
         if (!this.enableIncreaseOrDecrease(isIncrease)) {
             return;
         }
 
         const { step } = this.props;
         const { value } = this.controllableState.state;
-        const bn = BigNumber(formatNumberCode(value));
+        const bn = BigNumber(value);
         const newValue = isIncrease ? bn.plus(step as number) : bn.minus(step as number);
         this.onchangeValue(this.getValueNotOutOfRange(newValue.toFixed()));
     }
@@ -220,12 +324,51 @@ class InputNumber extends Component<Props, State> {
         };
     }
 
+    /**
+     * 格式化value值，如果有formatter，则使用formatter格式化
+     * @param value
+     * @protected
+     */
+    protected formatValue(value: string): string {
+        const { decimalSeparator, formatter } = this.props;
+        if (decimalSeparator) {
+            // 替换.为decimalSeparator
+            value = value.replace(/\./g, decimalSeparator);
+        }
+        if (formatter) {
+            value = formatter(value);
+        }
+
+        return value;
+    }
+
+    /**
+     * 解析value值，如果有parser，则使用parser解析
+     * @param value
+     * @protected
+     */
+    protected parse(value: string): string {
+        const { decimalSeparator, parser } = this.props;
+        if (decimalSeparator) {
+            // 替换decimalSeparator为. decimalSeparator可能为.或者其他
+            value = value.replace(new RegExp(decimalSeparator.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), '.');
+        }
+        if (parser) {
+            value = parser(value);
+        }
+        return value;
+    }
+
     setup(): void {
         useEffect(() => {
             if (this.props.autoFocus) {
                 this.inputRef.current?.focus();
             }
         }, () => [this.inputRef.current]);
+
+        useEffect(() => {
+            this.state.formatValue = this.formatValue(this.controllableState.state.value);
+        }, () => [this.controllableState.state.value]);
     }
 }
 
--- stories/examples/input-number/InputNumber.ts
+++ stories/examples/input-number/InputNumber.ts
@@ -7,20 +7,34 @@ export default class InputRoot extends Component {
     state = useState({
         disabled: false,
         size: 'middle',
-        border: true
+        step: 1,
+        border: true,
+        controls: true,
+        decimalSeparator: '.',
+        keyboard: true,
+        readonly: false
     });
 
     static template = xml`
 <div class="input-container">
-    <InputNumber max="4" min="0" placeholder="'Basic usage'" disabled="state.disabled" size="state.size" bordered="state.border">
-        <t t-set-slot="addonAfter">
-            <div>.com</div>
-        </t>
+    <InputNumber max="4" min="-20" placeholder="'InputNumber'" 
+        disabled="state.disabled" 
+        size="state.size" 
+        step="state.step"
+        controls="state.controls" 
+        bordered="state.border"
+        decimalSeparator="state.decimalSeparator"
+        keyboard="state.keyboard"
+        readonly="state.readonly"
+    >
         <t t-set-slot="addonBefore">
-            http://
+            金额
+        </t>
+        <t t-set-slot="addonAfter">
+            <div>整</div>
         </t>
         <t t-set-slot="prefix">
-            <div>$</div>
+            <div>￥</div>
         </t>
         <t t-set-slot="suffix">
             <div>元</div>
